d23b0c63bff0a9beaf427f9979730a164e7d5f1b
Anderson Alexsandro Soares <anderson.silva.04@outlook.com>
2023-08-01 12:09:03 -0300

    resizing Stack

--- src/main/java/io/github/andersonalexsandro/FirstModule/ResizingStack.java
+++ src/main/java/io/github/andersonalexsandro/FirstModule/ResizingStack.java
@@ -2,13 +2,13 @@ package io.github.andersonalexsandro.FirstModule;
 
 import java.lang.reflect.Array;
 
-public class FixedCapacityStack<T> {
+public class ResizingStack<T> {
     private Class<T> type;
     private T[] stack;
     private int size;
 
     @SuppressWarnings("unchecked")
-    public FixedCapacityStack(int length, Class<T> type) {
+    public ResizingStack(int length, Class<T> type) {
         this.type = type;
         this.stack = (T[]) Array.newInstance(type, length);
         this.size = 0;
@@ -21,11 +21,20 @@ public class FixedCapacityStack<T> {
     public void push(T item){
         stack[size] = item;
         size++;
+        if(size == stack.length) resize(stack.length * 2);
     }
 
     public T pop(){
+        if(isEmpty()) throw new IndexOutOfBoundsException();
         T item = stack[size];
         stack[size] = null;
+        if(size>0 && size == stack.length/2) resize(stack.length/2);
         return item;
+    }
+
+    public void resize(int lengh){
+        T[] newStack = (T[]) Array.newInstance(type, lengh);
+        for(int i = 0; i<newStack.length; i++) newStack[i] = stack[i];
+        this.stack = newStack;
     }
 }
