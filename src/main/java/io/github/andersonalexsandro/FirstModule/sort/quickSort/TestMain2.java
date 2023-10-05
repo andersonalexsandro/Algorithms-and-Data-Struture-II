b4c5e93243f82e4f33fb323527a817d214fe6c9b
Anderson Alexsandro Soares <anderson.silva.04@outlook.com>
2023-10-04 22:45:57 -0300

    some chances

--- src/main/java/io/github/andersonalexsandro/FirstModule/sort/quickSort/TestMain2.java
+++
@@ -1,9 +0,0 @@
-package io.github.andersonalexsandro.FirstModule.sort.quickSort;
-
-public class TestMain2 {
-    public static void main(String[] args) {
-
-        Integer[] numberToSort = {7, 2, 1, 8, 6, 3, 5, 4};
-        new QuickSort2(numberToSort).sort();
-    }
-}
